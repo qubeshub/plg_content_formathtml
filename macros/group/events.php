42ca93b9ff2c379ad0362010948219b6c8227245
M. Drew LaMar <mdlama@wm.edu>
2024-07-11 16:40:08 -0400

    [ref] Added functionality to group events macro
    Now handles repeating events, defaults to one year of events,
    timezones are handled correctly (shows events in users timezone), etc.
    
    (cherry picked from commit 8aa2af116b5b030c563f322cc1ec930e7ad87212)

--- macros/group/events.php
+++ macros/group/events.php
@@ -8,8 +8,10 @@
 namespace Plugins\Content\Formathtml\Macros\Group;
 
 require_once dirname(__DIR__) . DS . 'group.php';
+require_once PATH_APP . DS . 'components' . DS . 'com_events' . DS . 'models' . DS . 'calendar.php';
 
 use Plugins\Content\Formathtml\Macros\GroupMacro;
+use Components\Events\Models\Calendar;
 
 /**
  * Group events Macro
@@ -31,11 +33,14 @@ class Events extends GroupMacro
 	public function description()
 	{
 		$txt = array();
-		$txt['html']  = '<p>Displays group events.</p>';
+		$txt['html']  = '<p>Displays group events. All dates are in YYYY-MM-DD format.</p>';
 		$txt['html'] .= '<p>Examples:</p>
 							<ul>
-								<li><code>[[Group.Events()]]</code></li>
-								<li><code>[[Group.Events(3)]]</code> - Displays the next 3 group events</li>
+								<li><code>[[Group.Events()]]</code> - Displays all future events</li>
+								<li><code>[[Group.Events(from=2021-04-15)]]</code> - Displays events from 2021-04-15</li>
+								<li><code>[[Group.Events(from=2021-04-15, for=3 months)]]</code> - Displays 3 months of events from 2021-04-15 (can use days, weeks, months, years)</li>
+								<li><code>[[Group.Events(from=today, to=2025-01-01)]]</code> - Displays events from today to 2025-01-01</li>
+								<li><code>[[Group.Events(to=today)]]</code> - Displays events up to today (not inclusive)</li>
 							</ul>';
 
 		return $txt['html'];
@@ -58,9 +63,8 @@ class Events extends GroupMacro
 		$args = $this->getArgs();
 
 		// Array of filters
-		$filters = array(
-			'limit' => (isset($args[0]) && is_numeric($args[0])) ? $args[0] : 3
-		);
+		$filters = array();
+		echo var_dump($args);
 
 		// Get group events
 		$events =  $this->getGroupEvents($this->group, $filters);
@@ -87,28 +91,139 @@ class Events extends GroupMacro
 	 */
 	private function getGroupEvents($group, $filters = array())
 	{
-		// Instantiate database
-		$database = \App::get('db');
+		// Pulled from core/plugins/groups/calendar/calendar.php::events()
 
-		// Build query
-		$sql = "SELECT * FROM `#__events`
-				WHERE publish_up >= UTC_TIMESTAMP()
-				AND scope=" . $database->quote('group') . "
-				AND scope_id=" . $database->Quote($group->get('gidNumber')) . "
-				AND state=1";
+		// array to hold events
+		$events = array();
 
-		// Add ordering
-		$sql .= " ORDER BY publish_up ASC";
+		// get request params
+		$start      = isset($filters['start']) ? $filters['start'] : '';
+		$end        = isset($filters['end']) ? $filters['end'] : '';
+		$calendarId = isset($filters['calendar_id']) ? $filters['calendar_id'] : 0;
 
-		// Do we have a limit set
-		if (isset($filters['limit']))
+		if ($start && !preg_match('/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/', $start))
 		{
-			$sql .= " LIMIT " . $filters['limit'];
+			$start = '';
+		}
+		if ($end && !preg_match('/^([0-9]{4})-([0-9]{2})-([0-9]{2})$/', $end))
+		{
+			$end = '';
 		}
 
-		// Return result
-		$database->setQuery($sql);
-		return $database->loadObjectList();
+		// format date/times
+		$start = $start . ' 00:00:00';
+		$end   = '0000-00-00 00:00:00';
+		$until = Date::of('now')->add('1 year')->format('Y-m-d H:i:s');
+
+		// get calendar events
+		$eventsCalendar = \Components\Events\Models\Calendar::getInstance();
+		$rawEvents = $eventsCalendar->events('list', array(
+			'scope'        => 'group',
+			'scope_id'     => $this->group->get('gidNumber'),
+			'calendar_id'  => $calendarId,
+			'state'        => array(1),
+			'publish_up'   => $start, // $start->format('Y-m-d H:i:s'),
+			'publish_down' => $end, // $end->format('Y-m-d H:i:s'),
+			'non_repeating'    => true
+		));
+
+		// get repeating events
+		$rawEventsRepeating = $eventsCalendar->events('repeating', array(
+			'scope'        => 'group',
+			'scope_id'     => $this->group->get('gidNumber'),
+			'calendar_id'  => $calendarId,
+			'state'        => array(1),
+			'publish_up'   => $start, // $start->format('Y-m-d H:i:s'),
+			'publish_down' => $end, // $end->format('Y-m-d H:i:s')
+			'until'		   => $until // $end->format('Y-m-d H:i:s')
+		));
+
+		// merge events with repeating events
+		$rawEvents = $rawEvents->merge($rawEventsRepeating);
+
+		// loop through each event to return it
+		foreach ($rawEvents as $rawEvent)
+		{
+			$up   = Date::of($rawEvent->get('publish_up'));
+			$down = Date::of($rawEvent->get('publish_down'));
+			$params = new \Hubzero\Config\Registry($rawEvent->get('params'));
+			$ignoreDst = false;
+			$ignoreDst = $params->get('ignore_dst') == 1 ? true : false;
+
+			$event            = new \stdClass;
+			$event->title     = $rawEvent->get('title');
+
+			// Create URL to event
+			$event->url       = $rawEvent->link();
+			// add start & end for displaying dates user clicked on
+			// instead of actual event start & end
+			if ($rawEvent->get('repeating_rule') != '')
+			{
+				$event->url .= '?start=' . $up->toUnix();
+				if ($rawEvent->get('publish_down') && $rawEvent->get('publish_down') != '0000-00-00 00:00:00')
+				{
+					$event->url .= '&end=' . $down->toUnix();
+				}
+			}
+			$event->location = $rawEvent->get('adresse_info');
+			$event->about = nl2br($rawEvent->get('content'));
+			$strcap = 255;
+			if (strlen($event->about) > $strcap)
+			{
+				$event->about = \Hubzero\Utility\Str::truncate($event->about, $strcap, array('html' => true)) . ' <a href="' . $event->url . '">[more]</a>';
+			}
+
+			$event->allDay    = $rawEvent->get('allday') == 1;
+			$event->className = ($rawEvent->get('calendar_id')) ? 'calendar-' . $rawEvent->get('calendar_id') : 'calendar-0';
+			$event->start_month = $up->format('M');
+			$event->start_day = $up->format('d');
+			$event->iso_8601 = $up->format('c');
+			$event->year = $up->format('Y');
+			$event->up = $up; // Used for sorting below
+			if (!$event->allDay) {
+				// Is this an open-ended event?
+				if ($down <= Date::of('0000-00-00 00:00:00')) {
+					// Open-ended event
+					$event->start = $up->toLocal('g:i A T', $ignoreDst);
+					$event->end = "(heat death of the universe)";
+				} else {
+					if ($up->format('Y-m-d') == $down->format('Y-m-d')) {
+						// Single-day event
+						$event->start = $up->toLocal('g:i A', $ignoreDst);
+						$event->end = $down->toLocal('g:i A T', $ignoreDst);
+					} else {
+						// Multi-day event
+						$event->start = $up->toLocal('M j g:i A T', $ignoreDst);
+						$event->end = $down->toLocal('M j g:i A T', $ignoreDst);
+					}
+				}
+			} else {
+				// Adjustment for same-day all-day event
+				if (Date::of($down)->subtract('24 hours') <= $up)
+				{
+					$down = $up->add('24 hours');
+				}
+
+				// Is this an open-ended event?
+				if ($down <= Date::of('0000-00-00 00:00:00') || 
+				    $down->format('Y-m-d') != $up->format('Y-m-d')) {
+					$event->start = $up->format('M j');
+					$event->end = $down->format('M j');
+				} else {
+					$event->start = 'All day';
+					$event->end = '';
+				}
+			}
+
+			array_push($events, $event);
+		}
+
+		// Put in order of start datetime
+		uasort($events, function($a, $b) {
+			return $a->up->toUnix() - $b->up->toUnix();
+		});
+
+		return $events;
 	}
 
 	/**
@@ -120,48 +235,64 @@ class Events extends GroupMacro
 	 */
 	private function renderEvents($group, $events)
 	{
+		\Document::addStyleSheet(rtrim(str_replace(PATH_ROOT, '', __DIR__)) . DS . '../macro-assets' . DS . 'events' . DS . 'events.css');
+
 		$content = '';
+		$curr_year = '';
 		if (count($events) > 0)
 		{
 			foreach ($events as $event)
 			{
-				// Build link
-				$link = \Route::url('index.php?option=com_groups&cn=' . $group->get('cn') . '&active=calendar&action=details&event_id=' . $event->id);
-
-				// Build date
-				$date = '';
-				$publishUp   = $event->publish_up;
-				$publishDown = $event->publish_down;
-
-				if (date("z", strtotime($publishUp)) == date("z", strtotime($publishDown)))
-				{
-					$date  = \Date::of($publishUp)->toLocal('m/d/Y @ g:i a');
-					$date .= ' &mdash; ' . \Date::of($publishDown)->toLocal('g:i a');
+				if ($curr_year == '') {
+					$content .= '<div class="item-list">';
+					$content .= '<h3 class="year">' . $event->year . '</h3>';
+					$content .= '<ul class="list-unstyled">';
+					$curr_year = $event->year;
+				} elseif ($curr_year != $event->year) {
+					$content .= '</ul>';
+					$content .= '</div>';
+					$content .= '<div class="item-list">';
+					$content .= '<h3 class="year">' . $event->year . '</h3>';
+					$content .= '<ul class="list-unstyled">';
+					$curr_year = $event->year;
 				}
-				else if (isset($event->publish_down) && $event->publish_down && $event->publish_down != '0000-00-00 00:00:00')
+				$content .= '<li class="mb-4">';
+				$content .= '  <div>';
+				$content .= '    <div class="event-card_wrap">';
+				$content .= '      <div class="event-card_left">';
+				$content .= '        <div>';
+				$content .= '          <time class="event-date_stacked" datetime="' . $event->iso_8601 . '">';
+				$content .= '            <div class="event-date_stacked_month">' . $event->start_month . '</div>';
+				$content .= '            <div class="event-date_stacked_day">' . $event->start_day . '</div>';
+				$content .= '          </time>';
+				$content .= '        </div>';
+				$content .= '      </div>';
+				$content .= '      <div class="event-card_body">';
+				$content .= '        <div><h5><a href="' . $event->url . '">' . $event->title . '</a></h5></div>';
+				$content .= '        <div class="event-time event-time_teaser">';
+				$content .= '          <span>' . $event->start . '</span>';
+				if ($event->end)
 				{
-					$date  = \Date::of($publishUp)->toLocal('m/d/Y @ g:i a');
-					$date .= ' &mdash; ' . \Date::of($publishDown)->toLocal('m/d/Y @ g:i a');
+					$content .= '          <span>&nbsp;to&nbsp; </span><span>' . $event->end . '</span>';
 				}
-				else
-				{
-					$date  = \Date::of($publishUp)->toLocal('m/d/Y @ g:i a');
+				$content .= '        </div>';
+				if ($event->location) {
+					$content .= '        <div class="event-location location">';
+					if (preg_match_all('/(http|ftp|https):\/\/([\w-]+(?:(?:\.[\w_-]+)+))([\w.,@?()<>;^=%&:\/~+#-]*[\w@?(<^=%&\/~+#-])?/', $event->location)) {
+						$content .= '		  <a href="' . $event->location . '" target="_blank">' . $event->location . '</a>';
+					} else {
+						$content .= '		  <span>' . $event->location . '</span>';
+					}
+					$content .= '        </div>';
 				}
-
-				// Shorten content
-				$details = nl2br($event->content);
-				if (strlen($details) > 150)
-				{
-					$details = \Hubzero\Utility\Str::truncate($details, 150, array('html' => true));
-				}
-
-				// Create list
-				$content .= '<div class="event">';
-				$content .= '<strong><a class=" title" href="' . $link . '">' . stripslashes($event->title) . '</a></strong>';
-				$content .= '<br /><span class="date">' . $date . '</span>';
-				$content .= '<br /><span class="details">' . $details . '</span>';
-				$content .= '</div><br />';
+				$content .= '        <div>' . $event->about . '</div>';
+				$content .= '      </div>';
+				$content .= '    </div>';
+				$content .= '  </div>';
+				$content .= '</li>';
 			}
+			$content .= '</div>';
+			$content .= '</ul>';
 		}
 		else
 		{
